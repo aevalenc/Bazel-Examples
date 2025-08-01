2d4182ce0c3f4e9f3d34af627553a8e61d39349a
Alejandro Valencia <valencia.alex70@gmail.com>
2025-08-01 09:27:16 +0200

    Use utility methods in jacobi main

--- jacobi/main.cc
+++ jacobi/main.cc
@@ -3,8 +3,10 @@
  */
 
 #include "jacobi/jacobi.h"
+#include "jacobi/utils.h"
 #include <cstdint>
 #include <iostream>
+#include <time.h>
 
 int main()
 {
@@ -12,25 +14,48 @@ int main()
   std::cout << "\nStarting Jacobi iteration main...\n";
 
   // Example matrix and vectors for Jacobi iteration
-  const std::int32_t n = 7;
+  const std::int32_t n = 10001;
 
   // clang-format off
-  double A[n * n] = { 2, -1,  0,  0,  0,  0,  0,
-                     -1,  2, -1,  0,  0,  0,  0,
-                      0, -1,  2, -1,  0,  0,  0,
-                      0,  0, -1,  2, -1,  0,  0,
-                      0,  0,  0, -1,  2, -1,  0,
-                      0,  0,  0,  0, -1,  2, -1,
-                      0,  0,  0,  0,  0, -1,  2 };
+  const auto A = InitializeLaplaceMatrix(n);
   // clang-format on
 
-  double b[n] = {200, 0, 0, 0, 0, 0, 400};
-  double x[n] = {0, 0, 0, 0, 0, 0, 0};
+  double *b = new double[n];
+  for (std::int32_t i = 0; i < n; ++i)
+  {
+    if (i == 0)
+    {
+      b[i] = 200.0;
+    }
+    else if (i == n - 1)
+    {
+      b[i] = 400.0;
+    }
+    else
+    {
+      b[i] = 0.0;
+    }
+  }
+
+  double *x = new double[n];
+  for (std::int32_t i = 0; i < n; ++i)
+  {
+    x[i] = 0.0; // Initial guess
+  }
 
   const std::int32_t max_iter = 1000;
   const double tolerance = 1e-3;
 
+  const auto start = clock();
   JacobiIteration(A, b, x, n, max_iter, tolerance);
+  const auto end = clock();
+  const double elapsed_time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+  std::cout << "Jacobi iteration completed in " << elapsed_time
+            << " seconds.\n";
+
+  delete[] A;
+  delete[] b;
+  delete[] x;
 
   return 0;
 }
